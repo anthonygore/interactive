6c32cfbdd933a65d5a1fc684cb616f42fec378e1
Anthony Gore <anthony@vuejsdevelopers.com>
2022-05-25 19:10:55 +1000

    Adding project scaffold

---
+++ public/codetorial/tutorials/ref-reactive/steps/01/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const count = ref(0);
+    return {
+      count,
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ count }}</h1>
+</template>
