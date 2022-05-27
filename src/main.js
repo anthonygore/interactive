9a3f3c070cee51c7d962c5348716a103fce25b4a
Anthony Gore <anthony@vuejsdevelopers.com>
2022-05-27 10:59:46 +1000

    Updating GA data

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 window.addEventListener('page-view', ({ detail }) => {
-  if (window.dataLayer) {
-    window.dataLayer.push({
-      'event': 'virtualPageview',
-      'pageUrl': window.location.href,
-      'pageTitle': detail.title
-    });
-  }
+  window.dataLayer.push({
+    'event': 'virtualPageview',
+    'pageUrl': window.location.href,
+    'pageTitle': detail.title
+  });
 })
